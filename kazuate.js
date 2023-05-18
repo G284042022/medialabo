7e8fd5e63b9af69bb22a6fc7c275e4f052cf55b1
G284042022 <228404@st.takushoku-u.ac.jp>
2023-05-18 16:16:05 +0900

    kazuate

--- kazuate.js
+++ kazuate.js
@@ -1,9 +1,8 @@
 // 答え
-let kotae = Math.floor(Math.random()*10) + 1;
-console.log('答え（デバッグ用）: ' + kotae);
+
 
 // 入力回数（予想回数）
-let kaisu = 0;
+let kaisu = 3;
 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
@@ -18,18 +17,39 @@ function hantei() {
   let yoso = 4;
   
   // 課題3-1: 正解判定する
-  for(let i=0;i<4;i=i+1){
-    if(yoso==kotae){
-      console.log("おめでとう");
-      i=3;
-    }else if(yoso>kotae){
-      console.log("まちがい．答えはもっと小さいですよ");
-    }else if(yoso<kotae){
-      console.log("まちがい．答えはもっと大きいですよ");
+  let h=0;
+  let kotae;
+    for(let i=0;i<2;i++){
+      kotae = Math.floor(Math.random()*10) + 1;
+      console.log('答え（デバッグ用）: ' + kotae);
+      if(yoso==kotae){
+         i=2;
+         h=1;
+         console.log("正解です．おめでとう!");
+      }else if(yoso>kotae){
+        console.log("まちがい．答えはもっと小さいですよ");
+      }else if(yoso<kotae){
+        console.log("まちがい．答えはもっと大きいですよ");
+      }
     }
+    /*if(h===0){
+      kotae = Math.floor(Math.random()*10) + 1;
+      console.log('答え（デバッグ用）: ' + kotae);
+      if(yoso==kotae){
+          if(yoso!=kotae){
+            console.log("まちがい．残念でした答えは"+kotae+"です．");
+          }else {
+            console.log("正解です．おめでとう!");
+          }
+      }
+      */
+      
+      
+    }
+    
 
-  }
+  
   
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-}
+
