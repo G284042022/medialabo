3fef15e808bb1d9f64f08d4cd697ac40b00bc2e8
G284042022 <228404@st.takushoku-u.ac.jp>
2023-06-01 15:58:37 +0900

    project

--- weather.js
+++ weather.js
@@ -1,57 +1,69 @@
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
-
-
 let b = document.querySelector('#print');
-b.addEventListener('click', greeting);
-function greeting() {
-	let i = document.querySelector('input[name="chiku"]');
-	let chiku = i.value;
-	
+b.addEventListener('click',sendRequest);
+
+function sendRequest(){
+    let i = document.querySelector('input[name="basyo"]');
+    let key = i.value;
+    let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+key+'.json';
+    
+    // 通信開始
+    axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
 }
 
+// 通信が成功した時の処理
+function showResult(resp) {
+    // サーバから送られてきたデータを出力
+    let data = resp.data;
+
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+
+    }
+
+    // data をコンソールに出力
+    console.log(data);
+
+    // data.x を出力
+    console.log(data.x);
+
+
+
+
+    let names = data.name;
+    let a = document.querySelector('dd#names');
+    a.textContent = names;
+
+    for (let n of data.weather){
+        let weather = (n.description);
+    let b = document.querySelector('dd#weather');
+    b.textContent =  weather;
+    }
+    let temp_max = data.main.temp_max;
+    let c = document.querySelector('dd#temp_max');
+    c.textContent =  temp_max + '℃';
+    
+    let temp_min = data.main.temp_min;
+    let d = document.querySelector('dd#temp_min');
+    d.textContent =  temp_min + '℃';
+    
+
+
+
+}
+
+// 通信エラーが発生した時の処理
+function showError(err) {
+    console.log(err);
+}
+
+// 通信の最後にいつも実行する処理
+function finish() {
+    console.log('Ajax 通信が終わりました');
+    
+    
+}
 
